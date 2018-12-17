34975ab39041663b8c4cf6e4e32e9a3f10c92abb
Stevens Dorchies <Trocme11>
2018-12-17 12:05:13 +0100

    version1

--- index.php
+++ index.php
@@ -8,4 +8,19 @@
 
 // Notre premier Hello World
 
-     = " ";
+    $text = "Du texte est stocké<BR>";
+    echo"$text";
+
+    $y=30;
+    echo"y vaux&nbsp;$y<br>";
+
+    $m=5;
+    $n=7;
+    echo $m+$n."<br>";
+   echo strlen($text)."<br>";
+   echo str_word_count($text)."<br>";
+   echo strrev($text).'<br>';
+   echo strpos("hello world","world");
+   echo"<br>";
+   echo str_replace("world","tes nul",
+       "hello world");
